da3a8f4bcb1bb8a4508b1b6ff67b2d3855b08005
MZO9400 <hamza.hameed00@yahoo.com>
2021-06-06 19:04:03 +0500

    Question 1, 2

--- api/course_allocations.py
+++ api/course_allocations.py
@@ -15,3 +15,8 @@ def query(
     query_string = 'course_allocation({section}, {course}, {instructor}, {time}, {day}, {room})' \
         .format(course=course, section=section, instructor=instructor, time=time, day=day, room=room)
     return prolog.query(query_string)
+
+
+# Q1, Q2
+def schedule(class_name, day="DAY"):
+    return query(room=class_name, day=day)
