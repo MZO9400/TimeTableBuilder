8996f50edba704129c202ea380d668fd5f39c3bb
MZO9400 <hamza.hameed00@yahoo.com>
2021-06-14 13:49:25 +0500

    Add TimeTable subclass

--- graphics/main.py
+++ graphics/main.py
@@ -1,3 +1,6 @@
+import tkinter
+
+
 class TimeTableBuilder:
     def __init__(self, window, course_alloc, window_title="Time Table Builder"):
         self.window = window
@@ -6,3 +9,12 @@ class TimeTableBuilder:
         self.window.mainloop()
 
         self.sub_window = None
+
+    class TimeTable:
+        def __init__(self, parent, data):
+            self.window = tkinter.Tk()
+            self.parent = parent
+            self.data = data()
+
+            self.window.title('Time Table')
+            self.window.mainloop()
