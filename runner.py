2d8a49aef755ffbd48df169790ffc22c6cd39a07
MZO9400 <hamza.hameed00@yahoo.com>
2021-06-19 22:49:27 +0500

    Remove window variable from TimeTableBuilder call

--- runner.py
+++ runner.py
@@ -1,11 +1,6 @@
-from tkinter import Tk
-
 from api.course_allocations import PrologWrapper
 from graphics.main import TimeTableBuilder
 
 KNOWLEDGEBASE = "knowledgebase/course_allocations.pl"
 
-tt = TimeTableBuilder(
-    window=Tk(),
-    course_alloc=PrologWrapper(KNOWLEDGEBASE)
-).start()
+TimeTableBuilder(course_alloc=PrologWrapper(KNOWLEDGEBASE)).start()
